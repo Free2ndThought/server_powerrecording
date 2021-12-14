54123fa6125a51ccd442fc813ff824cc0853114f
Manuel <lehnerma@fim.uni-passau.de>
2021-12-14 13:50:54 +0100

    mapping configuration, beginning

---
+++ senor_mapping.py
@@ -0,0 +1,102 @@
+class Sensor:
+    name: str = ""
+
+
+class SensorMapping:
+    mapping: dict[str, str]
+
+    def __init__(self):
+        mapping = {}
+
+
+
+
+"JSON Mapping:" \
+"sensormapping:{" \
+"    sensor:{" \
+"       name: 'Stromsensor1-BLADL-02-005'," \
+"       ip: '192.168.188.100'," \
+"       domain: 'stromsensor1'}," \
+"    sensor:{" \
+"       name: 'Stromsensor2-BLADL-02-006'," \
+"       ip: '192.168.188.101'," \
+"       domain: 'stromsensor2'}," \
+"   sensor:{" \
+"       name: 'Stromsensor3-BLADL-02-007'," \
+"       ip: '192.168.188.102'," \
+"       domain: 'stromsensor3'}," \
+"   sensor:{" \
+"       name: 'Stromsensor4-BLADL-02-008'," \
+"       ip: '192.168.188.103'," \
+"       domain: 'stromsensor4'}," \
+"   sensor:{" \
+"       name: 'Stromsensor5-BLADL-02-009'," \
+"       ip: '192.168.188.104'," \
+"       domain: 'stromsensor5'}," \
+"   sensor:{" \
+"       name: 'Stromsensor6-BLADL-02-010'," \
+"       ip: '192.168.188.105'," \
+"       domain: 'stromsensor6'}," \
+"   sensor:{" \
+"       name: 'Stromsensor7-BLADL-02-004'," \
+"       ip: '192.168.188.106'," \
+"       domain: 'stromsensor7'}," \    #to here no WLAN configured
+"   sensor:{" \
+"       name: 'Stromsensor8-BLADL-03-009'," \
+"       ip: '192.168.188.107'," \
+"       domain: 'stromsensor8'}," \
+"   sensor:{" \
+"       name: 'Stromsensor9-BLADL-03-002'," \
+"       ip: '192.168.188.108'," \
+"       domain: 'stromsensor9'}," \
+"   sensor:{" \
+"       name: 'Stromsensor10-BLADL-03-001'," \
+"       ip: '192.168.188.109'," \
+"       domain: 'stromsensor10'}," \
+"   sensor:{" \
+"       name: 'Stromsensor11-BLADL-03-003'," \
+"       ip: '192.168.188.110'," \
+"       domain: 'stromsensor11'}," \
+"   sensor:{" \
+"       name: 'Stromsensor12-BLADL-03-004'," \
+"       ip: '192.168.188.111'," \
+"       domain: 'stromsensor12'}," \
+"   sensor:{" \
+"       name: 'Stromsensor13-BLADL-03-005'," \
+"       ip: '192.168.188.112'," \
+"       domain: 'stromsensor13'}," \
+"   sensor:{" \
+"       name: 'Stromsensor14-BLADL-03-007'," \
+"       ip: '192.168.188.113'," \
+"       domain: 'stromsensor14'}," \
+"   sensor:{" \
+"       name: 'Stromsensor15-BLADL-04-010'," \
+"       ip: '192.168.188.114'," \
+"       domain: 'stromsensor15'}," \
+"   sensor:{" \
+"       name: 'Stromsensor16-BLADL-04-009'," \
+"       ip: '192.168.188.115'," \
+"       domain: 'stromsensor16'}," \
+"   sensor:{" \
+"       name: 'Stromsensor17-BLADL-01-002'," \
+"       ip: '192.168.188.116'," \
+"       domain: 'stromsensor17'}," \
+"   sensor:{" \
+"       name: 'Stromsensor18-BLADL-01-003'," \
+"       ip: '192.168.188.117'," \
+"       domain: 'stromsensor18'}," \
+"   sensor:{" \
+"       name: 'Stromsensor19-BLADL-03-006'," \
+"       ip: '192.168.188.118'," \
+"       domain: 'stromsensor19'}," \
+"   sensor:{" \
+"       name: 'Stromsensor20-BLADL-04-001'," \
+"       ip: '192.168.188.119'," \
+"       domain: 'stromsensor20'}," \
+"   sensor:{" \
+"       name: 'Stromsensor21-BLADL-01-001'," \
+"       ip: '192.168.188.120'," \
+"       domain: 'stromsensor21'}," \
+
+
+
