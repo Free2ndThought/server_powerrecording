fe65eadd1e3ac61ff14038d633745569a62dfad9
Manuel <lehnerma@fim.uni-passau.de>
2022-03-26 13:10:23 +0100

    reverse producer

--- producer/pika_producer.py
+++ producer/pika_producer.py
@@ -1,72 +1,62 @@
 from os import environ
 
 if __name__ == "__main__":
-    try: #TODO remove after debug
-        if ('RABBIT_HOST' in environ):
-            rabbit_host = str(environ['RABBIT_HOST'])  # e.g 10.10.10.2
-        else:
-            rabbit_host = 'localhost'
-        import json
-        import zlib
-        import time
-        from queue import SimpleQueue
+    if ('RABBIT_HOST' in environ):
+        rabbit_host = str(environ['RABBIT_HOST'])  # e.g 10.10.10.2
+    else:
+        rabbit_host = 'localhost'
+    import json
+    import zlib
+    import time
+    from queue import SimpleQueue
 
-        import pika
+    import pika
 
-        from AllnetPoll import AllnetPoll
-        from local_config import SETUP_NR, DEVICE_LIST
-        from pooling import blocking_delay_generator, round_robin_pooling
-
-
-        print("Connecting to Allnet-Plugs ... ", )
-        devices = ["BLADL_0%d_0%02d" % (SETUP_NR, i) for i in DEVICE_LIST[SETUP_NR]]
-        assert len(devices)
-        q_list = [SimpleQueue() for device in devices]
-        thread_list = [AllnetPoll(dev, q) for dev, q in zip(devices, q_list)]
-        [thread.start() for thread in thread_list]
-        print("Sucessfully started all Allnet-Crawlers")
+    from AllnetPoll import AllnetPoll
+    from local_config import SETUP_NR, DEVICE_LIST
+    from pooling import blocking_delay_generator, round_robin_pooling
 
 
-        credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
-        for i in range(10):
-            try:
-                connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_host, port=5672, credentials=credentials))
-                break
-            except pika.exceptions.AMQPConnectionError:
-                print("Couldn't connect to RabbiMQ # ", i)
-                time.sleep(2)
+    print("Connecting to Allnet-Plugs ... ", )
+    devices = ["BLADL_0%d_0%02d" % (SETUP_NR, i) for i in DEVICE_LIST[SETUP_NR]]
+    assert len(devices)
+    q_list = [SimpleQueue() for device in devices]
+    thread_list = [AllnetPoll(dev, q) for dev, q in zip(devices, q_list)]
+    [thread.start() for thread in thread_list]
+    print("Sucessfully started all Allnet-Crawlers")
 
-        print("Established Connection to RabbitMQ Server")
-        channel = connection.channel()
 
-        channel.confirm_delivery()
+    credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
+    for i in range(10):
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_host, port=5672, credentials=credentials))
+            break
+        except pika.exceptions.AMQPConnectionError:
+            print("Couldn't connect to RabbiMQ # ", i)
+            time.sleep(2)
 
-        channel.queue_declare(queue='task_queue', durable=True)
+    print("Established Connection to RabbitMQ Server")
+    channel = connection.channel()
 
-        prop = pika.BasicProperties(content_type='application/json',
-                                    content_encoding='zlib',
-                                    delivery_mode=2,) # Non-persistent (1) or persistent (2).
+    channel.confirm_delivery()
 
-        print("Unixtime                 Power Measurements")
-        for timestamp in blocking_delay_generator(10):
-            messages = round_robin_pooling(q_list)
-            messages = list(messages)
-            if len(messages) > 0:
-                body = json.dumps(messages, ensure_ascii=False)
-                body = body.encode('utf-8')
-                body = zlib.compress(body)
-                channel.basic_publish(exchange='', routing_key='task_queue',
-                                      body=body, properties=prop, mandatory=True)
+    channel.queue_declare(queue='task_queue', durable=True)
 
-            print(timestamp, '\t', len(messages))
+    prop = pika.BasicProperties(content_type='application/json',
+                                content_encoding='zlib',
+                                delivery_mode=2,) # Non-persistent (1) or persistent (2).
 
-        assert False
+    print("Unixtime                 Power Measurements")
+    for timestamp in blocking_delay_generator(10):
+        messages = round_robin_pooling(q_list)
+        messages = list(messages)
+        if len(messages) > 0:
+            body = json.dumps(messages, ensure_ascii=False)
+            body = body.encode('utf-8')
+            body = zlib.compress(body)
+            channel.basic_publish(exchange='', routing_key='task_queue',
+                                  body=body, properties=prop, mandatory=True)
 
-#TODO remove after debug
-    except ModuleNotFoundError as mnfe:
-        import os
+        print(timestamp, '\t', len(messages))
 
-        files = [f for f in os.listdir('.') if os.path.isfile(f)]
-        for f in files:
-            print(f)
-        raise mnfe
+    assert False
