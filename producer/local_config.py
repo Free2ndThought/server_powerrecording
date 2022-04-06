8ed91aea48b6e297ad80c8c7e00f1596006c04c2
Manuel <lehnerma@fim.uni-passau.de>
2022-04-06 15:46:31 +0200

    remove error sensor in mapping, reaarange active devices

--- producer/local_config.py
+++ producer/local_config.py
@@ -4,7 +4,6 @@ devicelist = list(range(1,38))
 #devicelist.remove(4)
 #devicelist.remove(14)
 #devicelist.remove(30)
-devicelist.remove(32)
 DEVICE_LIST = [
     #[1,2,3,5,6,7,8,9,10],
     #[1,2,3,4,5,6,7,8,9,10],
