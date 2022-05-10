b7c4c2d0359abe7b7b02de6878a5415a51a2b63e
Manuel <lehnerma@fim.uni-passau.de>
2022-05-11 00:18:56 +0200

    Added notification bot as submodule, device name mapping added comment for historian introduced volume for grafana

--- producer/device_name_mapping.py
+++ producer/device_name_mapping.py
@@ -1,6 +1,7 @@
 # external IP FritzBox http://132.231.12.151/
 # ips 192.168.178. xxx are mapped to fritzbox:xxx
 # port 200 for CCU
+# port 199 for CCU historian
 
 HOSTNAME_TO_IP = {
     'BLADL_00_001' : '132.231.12.151:100',  #Sensor 1
